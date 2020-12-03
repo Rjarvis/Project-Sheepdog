1caaf54ab186c6c687a840a07b9884816ebe74cb
Roland J Jarvis <Rjarvis87@gmail.com>
2020-12-02 23:53:50 -0500

    Player can now look around with the mouse;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float lookSensitivity = 3f;
 
     private PlayerMotor motor;
 
@@ -27,6 +28,23 @@ public class PlayerController : MonoBehaviour
 
         //Apply movement
         motor.Move(_velocity);
+
+        //Calculate rotation as 3d vector: For turning around the player object
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(_rotation);
+
+        //Calculate camera rotation as 3d vector: For turning the player object's view up and down
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        //Apply camera rotation
+        motor.RotateCamera(_cameraRotation);
+
     }
 
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
+
+    [SerializeField] private Camera cam;
+
     private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
 
     private Rigidbody rb;
 
@@ -18,9 +23,29 @@ public class PlayerMotor : MonoBehaviour
         velocity = _velocity;
     }
 
+    public void Rotate(Vector3 _rotation) {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
     private void FixedUpdate()
     {
         PerformMovement();
+        PerformRotation();
+    }
+
+    //Perform rotation
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
     }
 
     //Perform movement based on velocity variable
