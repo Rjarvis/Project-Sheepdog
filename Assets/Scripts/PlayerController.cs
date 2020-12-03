2409beb4757544574770a7aa26ad60c820320e0f
Roland J Jarvis <Rjarvis87@gmail.com>
2020-12-02 23:34:17 -0500

    Player now moves; Deleted unrequired assets;

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        //Calculate movement velocity as a 3D vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMov; //(1,0,0)
+        Vector3 _moveVertical = transform.forward * _zMov; //(0,0,1)
+
+        //Final movement vector
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+
+        //Apply movement
+        motor.Move(_velocity);
+    }
+
+}
