004cba30795c69b2592358f1acda5b23c19b524c
Roland J Jarvis <Rjarvis87@gmail.com>
2020-12-04 03:11:07 -0500

    LobbyManager now created;  Can connect to Photon servers;

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class LobbyManager : MonoBehaviour
+public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] GameObject findMatchBtn, searchingPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        findMatchBtn.SetActive(false);
+        searchingPanel.SetActive(false);
+
+        //Connect to the photon Server
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
+        Debug.Log("Connection to Photon established!");
+        Debug.Log(" Connected on <color=blue>" + PhotonNetwork.CloudRegion + "</color> Server at "+ PhotonNetwork.GetPing() +"ms ping!");
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+        findMatchBtn.SetActive(true);
+    }
+
+    public void FindMatch()
+    {
+        searchingPanel.SetActive(true);
+        findMatchBtn.SetActive(false);
         
     }
 }
